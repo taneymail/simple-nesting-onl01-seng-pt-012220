bf4f0a86e32a02c029dc610b2d7bfd6c78b0a9c0
T S <taneymail@gmail.com>
2020-02-02 20:42:33 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -46,7 +46,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-programmer_hash[:dennis_ritchie][:languages][1]
+programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
