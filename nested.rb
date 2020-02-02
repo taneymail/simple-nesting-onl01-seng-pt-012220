266357cf29978742cd096c04b6e995f06585497d
T S <taneymail@gmail.com>
2020-02-02 20:33:40 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -1,11 +1,7 @@
 
 def hopper
 	programmer_hash = 
- 		{
-        :grace_hopper => {
-          :known_for => "COBOL",
-          :languages => ["COBOL", "FORTRAN"]
-        },
+ 		{:grace_hopper => {:known_for => "COBOL", :languages => ["COBOL", "FORTRAN"]},
         :alan_kay => {
           :known_for => "Object Orientation",
           :languages => ["Smalltalk", "LISP"]
@@ -16,7 +12,7 @@ def hopper
         }
      }
 
-
+programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
