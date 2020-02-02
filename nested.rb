332dfd2a32edf2abee4303726d960f9e18308d0c
T S <taneymail@gmail.com>
2020-02-02 20:47:02 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -69,10 +69,14 @@ def adding_matz
         :dennis_ritchie => {
           :known_for => "Unix",
           :languages => ["C"]
-        },
+        }
+ 		}
         
-        
-     }
+        programmer_hash[:yukihiro_matsumoto] = {}
+		 programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
+		 programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
+		 programmer_hash
+     
 
     
 end
